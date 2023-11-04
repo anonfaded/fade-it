fb3823c93907d818e46bc072ae4f591ec609a00b
Faded <124708903+anonfaded@users.noreply.github.com>
2023-11-04 17:08:30 +0500

    case-in

--- fadeit.py
+++ fadeit.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import re
 # Author information
 Tool = "Fade-It"
 Author = "Faded"
@@ -28,9 +29,43 @@ logo = """
 header = "============Censored Words Bypasser============"
 h2 = "==============================================="
 
+
+
 def change_text(input_text):
-    changed_text = input_text.replace('o', '0').replace('i', '!').replace('israel', '!sr@3l')
+    # Replace full words first
+    changed_text = input_text.replace('israel', '!sr@3l')
+    changed_text = changed_text.replace('palestine', 'p@|-est!n3')
+    changed_text = changed_text.replace('hamas', 'h@m-@s')
+    changed_text = changed_text.replace('palestine', 'p@|-est!n3')
+    changed_text = changed_text.replace('gaza', 'Ga-z@')
+    changed_text = changed_text.replace('palestine', 'p@|-est!n3')
+    changed_text = changed_text.replace('bomb', 'b0-mb')
+    changed_text = changed_text.replace('genocide', 'g3n0$!d3')
+    changed_text = changed_text.replace('falasteen', 'f@|ast!n3')
+    changed_text = changed_text.replace('terrorist', 't3rr0-r!$t')
+    changed_text = changed_text.replace('death', 'd3@-th')
+    changed_text = changed_text.replace('gun', 'gu-n')
+    changed_text = changed_text.replace('guns', 'gu-n$')
+    changed_text = changed_text.replace('terror', 't3rr-04')
+    changed_text = changed_text.replace('america', '@m3r!-c@')
+    
+       
+    
+
+    # Then, replace individual letters in a case-insensitive manner
+    changed_text = re.sub(r'o', '0', changed_text, flags=re.IGNORECASE)
+    changed_text = re.sub(r'i', '!', changed_text, flags=re.IGNORECASE)
+    changed_text = re.sub(r'a', '@', changed_text, flags=re.IGNORECASE)
+    changed_text = re.sub(r'e', '3', changed_text, flags=re.IGNORECASE)
+    changed_text = re.sub(r'l', '|', changed_text, flags=re.IGNORECASE)
+    changed_text = re.sub(r's', '$', changed_text, flags=re.IGNORECASE)
+
     return changed_text
+
+
+
+
+
 
 if __name__ == "__main__":
     # Print header and logo
@@ -44,11 +79,11 @@ if __name__ == "__main__":
     
 #    print("\n Bypassed text:\n\t", changed_result)
     while True:
-        user_input = input("\n\n Enter text (or press Enter to exit):\n   ")
+        user_input = input("\n\n - Enter text (or press Enter to exit):\n >>>  ")
         
         if user_input == "":
-            print("Goodbye!")
+            print("G00dby3!")
             break  # Exit the loop if the user presses Enter without input
         
         changed_result = change_text(user_input)
-        print("\n Bypassed text:\n  ", changed_result)
+        print("\n - Bypassed text:\n >>> ", changed_result)
